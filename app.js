10b532f7ea9166c08f2f11c998a9e459426a183a
hyerin oh <hyerinoh1108@gmail.com>
2019-03-26 18:52:44 +0900

    delete useless folder

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ app.get('/game', (req, res) => {
   res.send({ message: ok});
 });
 
-
 // 웹 소켓 서버 실행
 const socketio = require('socket.io');
 const io = socketio.listen(server);
@@ -24,68 +23,97 @@ let count = 0;
 server.listen(3001);
 const allClients = [];
 const userInfo = {};
+const rooms = {};
+let roomIndex = 0;
+
 io.on('connection', (socket) => {
-  // console.log('사용자 접속', socket.client.id);
-  socket.on('userName', (userNameAndPhto) => {
-    userInfo.name = userNameAndPhto.name;
-    userInfo.photo = userNameAndPhto.photo;
-    userInfo.id = socket.client.id;
-    console.log('userInfo',userInfo)
-    const copiedUserInfo = JSON.parse(JSON.stringify(userInfo));
-    if (allClients.length <= 2) {
-      count++;
-      allClients.push(copiedUserInfo);
-      io.emit('all', allClients);
-      socket.emit('order', count);
+  console.log('사용자 접속', socket.client.id);
+  socket.on('requestRoom', (userNameAndPhto) => {
+    console.log('!!!!!!!')
+    for (let roomId in rooms) {
+      if (rooms[roomId].userId.length === 1) {
+        socket.join(roomId);
+
+        rooms[roomId].userId.push(socket.client.id);
+        
+        userInfo[socket.client.id] = {
+          ...userNameAndPhto,
+          id: socket.client.id,
+          roomId: roomId
+        }
+
+        io.sockets.in(roomId).emit('completeMatch', 'game match');
+        console.log('11111111',userInfo[rooms[roomId].userId[1]])
+        io.to(rooms[roomId].userId[0]).emit('target', userInfo[rooms[roomId].userId[1]]);
+        io.to(rooms[roomId].userId[1]).emit('target', userInfo[rooms[roomId].userId[0]]);
+        return;
+      }      
     }
-    // console.log('all', allClients);
+
+    socket.join(`room${++roomIndex}`);
+
+    rooms[`room${roomIndex}`] = {
+      userId: [socket.client.id]
+    }
+
+    userInfo[socket.client.id] = {
+      ...userNameAndPhto,
+      id: socket.client.id,
+      roomId: `room${roomIndex}`
+    } 
   });
-  
-  socket.on('location', (location) => {
-    console.log(location);
-    console.log(allClients);
-    const forP1  = {};
-    const forP2 = {};
 
-    forP2.name = allClients[0].name;
-    forP2.id = allClients[0].id;
-    forP2.latitude = 37.491875;
-    forP2.longitude = 126.983863;
-    // forP2.latitude = location.latitude;
-    // forP2.longitude = location.longitude;
+  // socket.on('location', (location) => {
+  //   console.log(location);
+  //   console.log(allClients);
+  //   const forP1  = {};
+  //   const forP2 = {};
 
-    if (allClients.length === 2) {
-      forP1.name = allClients[1].name;
-      forP1.id = allClients[1].id;
-      forP1.latitude = 37.184631;
-      forP1.longitude = 127.121022;
+  //   forP2.name = allClients[0].name;
+  //   forP2.id = allClients[0].id;
+  //   // 임시값
+  //   forP2.latitude = 37.491875;
+  //   forP2.longitude = 126.983863;
+
+  //   // 실제 사용 코드
+  //   // forP2.latitude = location.latitude;
+  //   // forP2.longitude = location.longitude;
+
+  //   if (allClients.length === 2) {
+  //     forP1.name = allClients[1].name;
+  //     forP1.id = allClients[1].id;
+
+  //     //임시값
+  //     forP1.latitude = 37.184631;
+  //     forP1.longitude = 127.121022;
       
-      // forP1.latitude = location.latitude;
-      // forP1.longitude = location.longitude;
-    }
+  //     // 실제 사용 코드
+  //     // forP1.latitude = location.latitude;
+  //     // forP1.longitude = location.longitude;
+  //   }
 
-    if (Object.keys(forP1).length) {
-      io.to(forP2.id).emit('quiz', forP1);
-      io.to(forP1.id).emit('quiz', forP2);
-    }
+  //   if (Object.keys(forP1).length) {
+  //     io.to(forP2.id).emit('quiz', forP1);
+  //     io.to(forP1.id).emit('quiz', forP2);
+  //   }
     
-  })
+  // })
 
-  socket.on('winner', (winner) => {
-    io.emit('who', winner)
-  })
+  // socket.on('winner', (winner) => {
+  //   io.emit('who', winner)
+  // })
 
-   socket.on('disconnect', (reason) => {
-      if (reason === 'transport close') {
-        count--;
-        console.log('Got disconnect!');
-        console.log('count', count)
-        const i = allClients.indexOf(socket.client.id);
-        allClients.splice(i, 1);
-      }
-      console.log(socket.client.id, '<<< 여기여기');
-      console.log(allClients)
-   });
+  //  socket.on('disconnect', (reason) => {
+  //     if (reason === 'transport close') {
+  //       count--;
+  //       console.log('Got disconnect!');
+  //       console.log('count', count)
+  //       const i = allClients.indexOf(socket.client.id);
+  //       allClients.splice(i, 1);
+  //     }
+  //     console.log(socket.client.id, '<<< 여기여기');
+  //     console.log(allClients)
+  //  });
 });
 
 // ip : 192.168.0.53
